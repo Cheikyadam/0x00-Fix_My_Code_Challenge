9f23cd9c1246985465c69c663edea024571461c0
Cheikyadam <urbaingande@gmail.com>
2024-03-18 17:49:46 -0700

    square upd

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
+"""my square"""
 
-class square():
-    
+
+class square:
+    """class definition"""
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
+        """init here"""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -15,10 +17,13 @@ class square():
         return self.width * self.height
 
     def PermiterOfMySquare(self):
+        """Perimeter"""
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """str func"""
         return "{}/{}".format(self.width, self.height)
+
 
 if __name__ == "__main__":
 
